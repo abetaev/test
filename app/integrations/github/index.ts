8bca34f5846fd2bad96073332aa88121daa413f1
Alexander Betaev <betaev@gmail.com>
2023-11-19 21:43:45 -0600

    github ops

--- app/integrations/github/index.ts
+++ app/integrations/github/index.ts
@@ -1,28 +1,54 @@
-import { request } from 'graphql-request'
-import { Discussion, GetDiscussionsDocument } from '../graphql'
+import { GraphQLClient, request } from 'graphql-request'
+import { Discussion, DiscussionCategory, GetDiscussionCategoriesDocument, GetDiscussionsDocument, NewDiscussionDocument } from '../graphql'
+import _omit from 'lodash/omit'
 
 type Repository = {
   discussions(): Promise<Discussion[]>
+  discussionCategories(): Promise<(DiscussionCategory)[]>
+  newDiscussion(repositoryId: string, categoryId: string, title: string, body: string): Promise<Discussion>
 }
 
 const url = "https://api.github.com/graphql"
 
-export default (user: string, repo: string, token: string): Repository => {
-  
-  return {
-    discussions: async () => {
-      const response = await request({
-        url,
-        document: GetDiscussionsDocument,
-        variables: {
-          user,
-          repo,
-        },
-        requestHeaders: {
-          "Authorization": token
-        }
-      })
-      return response.repository?.discussions?.edges?.map(edge => edge?.node as Discussion) ?? []
+type Props = { user: string, repo: string, token: string }
+export default (props: Props): Repository => {
+  const client = new GraphQLClient(url, {
+    headers: {
+      "Authorization": props.token
     }
+  })
+  const variables = _omit(props, "token")
+  return {
+    discussions: () => client.request({
+      document: GetDiscussionsDocument,
+      variables,
+    }).then(
+      response => response
+        .repository
+        ?.discussions
+        ?.edges
+        ?.map(edge => edge?.node as Discussion) ?? []
+    ),
+    discussionCategories: () => client.request({
+      document: GetDiscussionCategoriesDocument,
+      variables,
+    }).then(
+      response => response
+        ?.repository
+        ?.discussionCategories
+        ?.nodes
+        ?.map(node => node as DiscussionCategory) ?? []
+    ),
+    newDiscussion: (repositoryId, categoryId, title, body) => client.request({
+      document: NewDiscussionDocument,
+      variables: {
+        repositoryId,
+        categoryId,
+        title,
+        body
+      }
+    }).then(
+      response => response?.createDiscussion?.discussion as Discussion
+    )
   }
 }
--- app/layout/index.tsx
+++ app/layout/index.tsx
@@ -1,26 +1,49 @@
 import GithubAuth from '~/integrations/github/auth';
 import GithubClient from '~/integrations/github';
 import { For, createEffect, createSignal } from 'solid-js';
-import { Discussion } from '~/integrations/graphql';
+import { Discussion, DiscussionCategory } from '~/integrations/graphql';
 
 export default () => {
   return (
     <GithubAuth>
       {token => {
-        const client = GithubClient("abetaev", "test", token);
+        const client = GithubClient({ user: "abetaev", repo: "test", token });
         const [getDiscussions, setDiscussions] = createSignal<Discussion[]>();
+        const [getDiscussionCategories, setDiscussionCategories] = createSignal<DiscussionCategory[]>()
         client.discussions().then(setDiscussions)
+        client.discussionCategories().then(setDiscussionCategories)
         return (
-          <For each={getDiscussions()}>
-            {discussion => (
-              <article>
-                <header>
-                  <h1>{discussion.title}</h1>
-                </header>
-                <main>{discussion.body}</main>
-              </article>
-            )}
-          </For>
+          <>
+            <For each={getDiscussions()}>
+              {discussion => (
+                <article>
+                  <header>
+                    <h1>{discussion.title}</h1>
+                  </header>
+                  <main>{discussion.body}</main>
+                </article>
+              )}
+            </For>
+            <hr />
+            <ul>
+              <For each={getDiscussionCategories()}>
+                {category => {
+                  var input = <input/> as HTMLInputElement
+                  var textarea = <textarea/> as HTMLTextAreaElement
+                  return (
+                    <li>
+                      {input}
+                      {textarea}
+                      <button onClick={() => {
+                        client.newDiscussion(category.repository.id, category.id, input.value, textarea.value)
+                      }}>{category.name}</button>
+                    </li>
+                  )
+                }
+                }
+              </For>
+            </ul>
+          </>
         )
       }}
     </GithubAuth>
